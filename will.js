ff2fa3424dc67deaf993c39ad8f8eaf0f0135244
Ldevito <ldevitoesq@gmail.com>
2016-12-11 15:47:16 -0500

    change

--- will.js
+++ will.js
@@ -1,3 +1,66 @@
 <script>
-var myWill = "test"
+var delay=2000; //700 = 1 second
+
+setTimeout(function() {
+var a = responsiveVoice.speak("What is your client's name");
+}, delay);
+
+function cityName(){
+    var a = responsiveVoice.speak("What city does client live in");
+}
+function countyName(){
+    var a = responsiveVoice.speak("What county does client live in");
+}
+function child1Name()
+{
+    var a = responsiveVoice.speak("What is you childs name");
+}
+function myFunction(){
+x=document.getElementsByClassName("Name");  // Find the elements
+for(var i = 0; i < x.length; i++){
+x[i].innerText="Name";    // Change the content
+}
+}
+myFunction();
+     // check if speech recognition is supported
+    if (annyang) {
+    // Define your commands
+    var commands = {
+    //  'My name is *val': function(val) { var first = val;
+    //  var setFirst = document.getElementById('firstName');
+    //  setFirst.textContent = first;
+    //  speak();
+//},
+    'Clients name is *val': function(val) {
+    var Second = val;
+    setTest = document.getElementsByClassName("Name");
+    for(var i = 0; i < x.length; i++){
+    setTest[i].innerText = Second;
+    }
+    cityName();
+},
+    'Client lives in *val': function(val) {
+     var cityName = val;
+     countyName();
+     setTest = document.getElementsByClassName("cityName");
+     for(var i = 0; i < x.length; i++){
+     setTest[i].innerText = cityName;
+   }
+
+},
+    'Clients county is *val': function(val) {
+     var countyName = val;
+     setTest = document.getElementsByClassName("countyName");
+     child1Name();
+     for(var i = 0; i < x.length; i++){
+     setTest[i].innerText = countyName;
+}
+}
+    };
+
+    // Use addCommands API to add commands to annyang
+    annyang.addCommands(commands);
+    // Start listening. You call this right here or do it later on some event like button click
+    annyang.start();
+    }
 </script>
